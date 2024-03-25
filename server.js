4519a72ecd31d795240175e2e2c2c4797cce6b80
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-25 16:19:27 +0100

    test

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require("./app");
-const { connectToMongoose } = require("./config/mongoose");
+const app = require("./api/src/app");
+const { connectToMongoose } = require("./api/src/config/mongoose");
 
 
 const port = process.env.PORT || 8000

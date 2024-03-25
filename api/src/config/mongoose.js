625bded8794a33aa76ac5c88edf0d242e1693aab
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-25 15:53:01 +0100

    change Uri name

--- api/src/config/mongoose.js
+++ api/src/config/mongoose.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
-const databaseURI = process.env.MONGOOSE_LIVE_URI;
+const databaseURI = process.env.MONGODB_URI;
 
 function connectToMongoose() {
-    mongoose.connect(databaseURI)
-        .then(() => {
-            console.log("Connected to DB: ", databaseURI)
-        })
-        .catch((error) => {
-            console.log("Could not connect to DB: ", databaseURI, error)
-        })
+  mongoose
+    .connect(databaseURI)
+    .then(() => {
+      console.log("Connected to DB: ", databaseURI);
+    })
+    .catch((error) => {
+      console.log("Could not connect to DB: ", databaseURI, error);
+    });
 }
 
 module.exports = {
-    connectToMongoose
-}
+  connectToMongoose,
+};

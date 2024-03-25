7902abb459f8ca22f1d25880d2fdf81269e1469f
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-25 14:07:22 +0100

    added base server request

--- api/src/app.js
+++ api/src/app.js
@@ -13,6 +13,12 @@ app.use(cors({
     origin: ["http://localhost:3000", "http://127.0.0.1:3000"]
 }))
 
+app.get("/", (req, res)  => {
+    res.json({
+        message: "Library API"
+    })
+})
+
 app.use("/api/v1/authors", authorRouter);
 app.use("/api/v1/books", bookRouter);
 

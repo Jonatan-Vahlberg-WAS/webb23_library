f6d27d3bf5ffbd43e3b1eb110311a725ab913bd9
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-04-25 11:48:36 +0200

    add author tests

--- api/__tests__/author.unit.test.js
+++ api/__tests__/author.unit.test.js
@@ -8,6 +8,15 @@ const dummyUser = {
     email: "jhon.doe@email.com",
     password: "password123"
 }
+
+const dummyAuthor = {
+    firstName: 'John',
+    lastName: 'Doe',
+    yearOfBirth: 1990,
+    primaryGenre: 'Fiction',
+    createdBy: undefined
+};
+
 let user1;
 
 describe('Author model tests', () => {
@@ -26,20 +35,59 @@ describe('Author model tests', () => {
 
 
     it('should create a new author', async () => {
-        const dummyAuthor = {
-            firstName: 'John',
-            lastName: 'Doe',
-            yearOfBirth: 1990,
-            primaryGenre: 'Fiction'
-        };
-        const author = await Author.create(dummyAuthor);
-
-        //TODO: finish test
+        const author = await Author.create({
+            ...dummyAuthor,
+            createdBy: user1?._id
+        });
+        expect(author.firstName).toBe("John")
     });
 
-    //TODO: should not create an author without a user
+    it('should not create a new author without a user', async () => {
 
-    //TODO: test other required fields
+        try {
+            const author = await Author.create({
+                ...dummyAuthor,
+                createdBy: undefined
+            });
+        } catch (error) {
+            expect(error.message).toMatch("Path `createdBy` is required")
+        }
+    });
+
+    it('should not create a author without required fields', async function() {
+        try {
+            const author = await Author.create({
+                ...dummyAuthor,
+                createdBy: user1?._id,
+                firstName: undefined,
+                lastName: undefined,
+                yearOfBirth: undefined,
+                primaryGenre: undefined
+
+            });
+        } catch (error) {
+            expect(error.message).toMatch("Author validation failed")
+            expect(error.message).toMatch("firstName")
+            expect(error.message).toMatch("lastName")
+            expect(error.message).toMatch("yearOfBirth")
+            expect(error.message).toMatch("primaryGenre")
+        }
+    })
+
+    it('should not create a author with invalid yearOfBirth: less than min ', async function() {
+        try {
+            const author = await Author.create({
+                ...dummyAuthor,
+                createdBy: user1?._id,
+                yearOfBirth: 159
+            });
+            expect(author.yearOfBirth).toBe(1602)
+        } catch (error) {
+            expect(error.message).toMatch("yearOfBirth: Path `yearOfBirth`")
+            expect(error.message).toMatch("is less than minimum allowed value")
+        }
+    })
+
 
     //TODO: test author cannot be created with invalid yearOFBirth
 });
